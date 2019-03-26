d78c89e7141276ab6b2f2f02e913f01e140d3d90
Jan Erik Hanssen <jhanssen@gmail.com>
2019-03-26 11:18:51 -0700

    rename to htaccess, only set headers if the header module is enabled

--- scripts/mklinks.sh
+++ scripts/mklinks.sh
@@ -4,7 +4,9 @@ for i in pie-chart logs compilers config; do
     ln -sf ./index.html "$i"
 done
 echo 'FileETag None
+<ifModule mod_headers.c>
 Header unset ETag
 Header set Cache-Control "max-age=0, no-cache, no-store, must-revalidate"
 Header set Pragma "no-cache"
-Header set Expires "Wed, 11 Jan 1984 05:00:00 GMT"' > ./.htaccess
+Header set Expires "Wed, 11 Jan 1984 05:00:00 GMT"
+</ifModule>' > ./htaccess
